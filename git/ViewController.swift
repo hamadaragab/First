9974e4b3d8a90beb47613bc03a28b86aac3b32ae
HAMADA <hamada@HAMADAs-MacBook-Pro.local>
2020-06-27 04:33:53 +0200

    gitHub added

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello GitHub")
+        print("gitHub added")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
